6e4930d8a8ca267e8007aa7b629f7fae25aec581
Matthias Beerens <3512339+Matthiee@users.noreply.github.com>
2021-01-10 10:09:07 +0100

    Starting point

--- ConsoleApp1/Models/GlobalOptions.cs
+++ ConsoleApp1/Models/GlobalOptions.cs
@@ -6,14 +6,5 @@ namespace UsersManagementApp.Models
     {
         [Name("v", "verbose"), DefaultValue(false), Description("Verbose output")]
         public bool Verbose { get; set; }
-
-        [Name("a"), DefaultValue(100), Description("Something a")]
-        public int A { get; set; }
-
-        [Name("b"), DefaultValue(200), Description("Something b")]
-        public int B { get; set; }
-
-        [Name("c"), DefaultValue(300), Description("Something c")]
-        public int C { get; set; }
     }
 }
